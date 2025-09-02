79e3052ab41795b6114d5e9400784a7480099f1c
Spaghetto-regretto <sarahahmadmnt@gmail.com>
2025-09-03 09:49:05 +1000

    Set up variables / constants for css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-async function getData() {
-  const url = "https://api.collection.nfsa.gov.au/search?query=dog";
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
+// async function getData() {
+//   const url = "https://api.collection.nfsa.gov.au/search?query=dog";
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(`Response status: ${response.status}`);
+//     }
 
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-getData(); 
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// }
+// getData(); 
